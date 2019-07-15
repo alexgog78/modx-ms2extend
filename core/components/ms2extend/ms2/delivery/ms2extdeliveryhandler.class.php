02fd0e176e5c45a5592bca809d5e4baff62d26e2
Aleksey <raven-98@yandex.ru>
2019-07-15 19:09:10 +0300

    Custom minishop2 handlers

---
+++ core/components/ms2extend/ms2/delivery/ms2extdeliveryhandler.class.php
@@ -0,0 +1,9 @@
+<?php
+if (!class_exists('msDeliveryInterface')) {
+	require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/minishop2/model/minishop2/msdeliveryhandler.class.php';
+}
+
+class ms2extDeliveryHandler extends msDeliveryHandler implements msDeliveryInterface
+{
+	
+}
