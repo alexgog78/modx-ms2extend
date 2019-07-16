5f4987115d822ab2fb67705a04500cec4604759c
Aleksey <raven-98@yandex.ru>
2019-07-16 13:19:51 +0300

    Context initialization

--- core/components/ms2extend/model/ms2extend/ms2extend.class.php
+++ core/components/ms2extend/model/ms2extend/ms2extend.class.php
@@ -2,6 +2,7 @@
 class ms2Extend {
 	public $modx;
 	public $config = array();
+	public $pdoTools;
 	public $initialized = array();
 	public $mgrLayoutHandler;
 
@@ -26,6 +27,10 @@ class ms2Extend {
 		
 		$this->modx->addPackage('ms2extend', $this->config['modelPath']);
 		$this->modx->lexicon->load('ms2extend:default');
+		
+		if ($this->pdoTools = $this->modx->getService('pdoFetch')) {
+			$this->pdoTools->setConfig($this->config);
+		}
 	}
 	
 	
@@ -37,36 +42,28 @@ class ms2Extend {
 			return true;
 		}
 		
-		//Add JS and CSS
 		switch($ctx){
 			case 'mgr':
-				if($this->modx->controller){
-					$this->modx->controller->addHtml('
-						<script type="text/javascript">
-							ms2Extend = {};
-							ms2Extend.config = ' . $this->modx->toJSON($this->config) . ';
-						</script>'
-					);
-				}
+				$this->initializeBackend();
 			break;
 			default:
-				$config_js = preg_replace(array('/^\n/', '/\t{5}/'), '', '
-					ms2ExtendConfig = {};
-					ms2ExtendConfig = {
-						cssUrl: "'.$this->config['cssUrl'] . 'web/",
-						jsUrl: "'.$this->config['jsUrl'] . 'web/",
-						actionUrl: "'.$this->config['actionUrl'].'"
-					};');
-				$this->modx->regClientStartupScript("<script type=\"text/javascript\">\n" . $config_js . "\n</script>", true);
-				
-				$config = $this->makePlaceholders($this->config);
-				if ($css = $this->modx->getOption('ms2ext_frontend_css')) {
-					$this->modx->regClientCSS(str_replace($config['placeholder'], $config['value'], $css));
-				}
-				if ($js = $this->modx->getOption('ms2ext_frontend_js')) {
-					$this->modx->regClientScript(str_replace($config['placeholder'], $config['value'], $js));
-				}
+				$this->initializeFrontend();
 			break;
+		}
+
+		$this->initialized[$ctx] = true;
+	}
+	
+	
+	public function initializeBackend() {
+		//Add JS and CSS
+		if($this->modx->controller){
+			$this->modx->controller->addHtml('
+				<script type="text/javascript">
+					ms2Extend = {};
+					ms2Extend.config = ' . $this->modx->toJSON($this->config) . ';
+				</script>'
+			);
 		}
 		
 		//MGR layout handler
@@ -76,22 +73,24 @@ class ms2Extend {
 			$this->modx->log(modX::LOG_LEVEL_ERROR, 'Could not initialize ms2extMgrLayoutHandler class');
 			return false;
 		}
-		
-		$this->initialized[$ctx] = true;
 	}
 	
 	
-	//Creating placeholders array
-	public function makePlaceholders(array $array = array()) {
-		$result = array('placeholder' => array(), 'value' => array());
-		foreach ($array as $key=>$value) {
-			if (is_array($value)) {
-				$result = array_merge_recursive($result, $this->makePlaceholders($value, $key . '.'));
-			} else {
-				$result['placeholder'][$key] = '[[+'.$key.']]';
-				$result['value'][$key] = $value;
-			}
+	public function initializeFrontend() {
+		//Add JS and CSS
+		$configJs = $this->modx->toJSON(array(
+			'cssUrl' => $this->config['cssUrl'] . 'web/',
+			'jsUrl' => $this->config['jsUrl'] . 'web/',
+			'actionUrl' => $this->config['actionUrl']
+		));
+		$this->modx->regClientStartupScript('<script type="text/javascript">ms2ExtendConfig = ' . $configJs . ';</script>', true);
+		
+		$config = $this->pdoTools->makePlaceholders($this->config);
+		if ($frontendCss = $this->modx->getOption('ms2ext_frontend_css')) {
+			$this->modx->regClientCSS(str_replace($config['pl'], $config['vl'], $frontendCss));
 		}
-		return $result;
+		if ($frontendJs = $this->modx->getOption('ms2ext_frontend_js')) {
+			$this->modx->regClientScript(str_replace($config['pl'], $config['vl'], $frontendJs));
+		}
 	}
 }
