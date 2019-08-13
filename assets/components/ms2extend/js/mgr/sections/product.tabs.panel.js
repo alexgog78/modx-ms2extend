a6d80b75b2c9509d0e9dbabb5e8f632f17ea60ef
Aleksey <raven-98@yandex.ru>
2019-08-13 16:28:10 +0300

    Extjs.panel

--- assets/components/ms2extend/js/mgr/sections/product.tabs.panel.js
+++ assets/components/ms2extend/js/mgr/sections/product.tabs.panel.js
@@ -5,46 +5,19 @@ Ext.onReady(function () {
 });
 ms2Extend.panel.groups = function (config) {
     config = config || {};
-    Ext.apply(config, {
-        //border: false,
-        //baseCls: 'modx-formpanel',
-        cls: 'container',
-        items: [{
-            html: '<h2>' + _('ms2extend.section.product-tabs') + '</h2>',
-            //border: false,
-            cls: 'modx-page-header'
-        }, {
-            xtype: 'modx-tabs',
-            /*defaults: {
-                border: false,
-                autoHeight: true
-            },*/
-            //border: true,
-            stateful: true,
-            stateId: 'jpayments-panel-payments-vtabs',
-            stateEvents: ['tabchange'],
-            getState: function () {
-                return {
-                    activeTab:this.items.indexOf(this.getActiveTab())
-                };
-             },
-            items: [{
-                title: _('ms2extend.tab.product-tabs'),
-                //defaults: {autoHeight: true},
-                layout: 'anchor',
-                items: [{
-                    html: '<p>' + _('ms2extend.tab.product-tabs.management') + '</p>',
-                    //border: false,
-                    bodyCssClass: 'panel-desc'
-                }, {
-                    xtype: 'ms2extend-grid-product-tabs',
-                    cls: 'main-wrapper',
-                    //preventRender: true
-                }]
-            }]
-        }]
-    });
+    if (!config.id) {
+        config.id = 'ms2extend-panel-product-tabs';
+    }
+    Ext.apply(config, {});
     ms2Extend.panel.groups.superclass.constructor.call(this, config);
 };
-Ext.extend(ms2Extend.panel.groups, MODx.Panel);
+Ext.extend(ms2Extend.panel.groups, abstractModule.panel.abstract, {
+    pageHeader: _('ms2extend.section.product-tabs'),
+
+    panelTabs: [{
+        title: _('ms2extend.tab.product-tabs'),
+        description: _('ms2extend.tab.product-tabs.management'),
+        xtype: 'ms2extend-grid-product-tabs',
+    }]
+});
 Ext.reg('ms2extend-panel-product-tabs', ms2Extend.panel.groups);
