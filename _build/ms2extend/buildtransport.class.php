177c4045240ca8f99f6d08127d5c126eaa5b5a33
Aleksey <a.goguev@alexgog.ru>
2020-02-25 23:04:56 +0300

    Working on package command

---
+++ _build/ms2extend/buildtransport.class.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__FILE__) . '/config.inc.php';
+
+class BuildTransport extends abstractCommand
+{
+    public function run()
+    {
+        // TODO: Implement run() method.
+
+        $this->log(111);
+    }
+}
