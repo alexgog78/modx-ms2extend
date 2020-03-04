50dfa6e2e4b75edce3ab58e8505181e90a39e089
Aleksey <a.goguev@alexgog.ru>
2020-03-04 22:07:42 +0300

    Version fix, new package

--- _build/ms2extend/config.inc.php
+++ _build/ms2extend/config.inc.php
@@ -2,5 +2,5 @@
 
 define('PKG_NAME', 'ms2Extend');
 define('PKG_NAME_LOWER', 'ms2extend');
-define('PKG_VERSION', '1.0');
-define('PKG_RELEASE', 'beta1');
+define('PKG_VERSION', '1.0.0');
+define('PKG_RELEASE', 'beta');
